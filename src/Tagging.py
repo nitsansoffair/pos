7fdaf9ecb5941b5b45836881df274d744748b67c
nitsan <nitsan.soffair@gmail.com>
2022-02-20 16:33:58 +0200

    create_dictionaries

--- src/Tagging.py
+++ src/Tagging.py
@@ -1,3 +1,21 @@
+from collections import defaultdict
+from src.utilities.utils_pos import get_word_tag, preprocess
+
 class Tagging:
     def __init__(self):
         pass
+
+    def create_dictionaries(self, training_corpus, vocab, verbose=False):
+        emission_counts, transition_counts, tag_counts = defaultdict(int), defaultdict(int), defaultdict(int)
+        prev_tag = '--s--'
+        i = 0
+        for word_tag in training_corpus:
+            i += 1
+            if i % 50000 == 0 and verbose:
+                print(f"word count = {i}")
+            word, tag = get_word_tag(word_tag, vocab)
+            transition_counts[(prev_tag, tag)] += 1
+            emission_counts[(tag, word)] += 1
+            tag_counts[tag] += 1
+            prev_tag = tag
+        return emission_counts, transition_counts, tag_counts
---
+++ src/tests/Tagging.py
@@ -0,0 +1,88 @@
+import unittest
+from collections import defaultdict
+
+import numpy as np
+
+from src.Tagging import Tagging
+
+
+class TaggingTest(unittest.TestCase):
+    def test_create_dictionaries(self):
+        tagging = Tagging()
+        target = tagging.create_dictionaries
+        with open("../../data/WSJ_02-21.pos", 'r') as f:
+            training_corpus = f.readlines()
+        with open("../../data/hmm_vocab.txt", 'r') as f:
+            voc_l = f.read().split('\n')
+        vocab = {}
+        for i, word in enumerate(sorted(voc_l)):
+            vocab[word] = i
+        test_cases = [
+            {
+                "name": "default_case",
+                "input": {
+                    "training_corpus": training_corpus,
+                    "vocab": vocab,
+                    "verbose": False,
+                },
+                "expected": {
+                    "len_emission_counts": 31140,
+                    "len_transition_counts": 1421,
+                    "len_tag_counts": 46,
+                    "emission_counts": {
+                        ("DT", "the"): 41098,
+                        ("NNP", "--unk_upper--"): 4635,
+                        ("NNS", "Arts"): 2,
+                    },
+                    "transition_counts": {
+                        ("VBN", "TO"): 2142,
+                        ("CC", "IN"): 1227,
+                        ("VBN", "JJR"): 66,
+                    },
+                    "tag_counts": {"PRP": 17436, "UH": 97, ")": 1376, },
+                },
+            },
+            {
+                "name": "small_case",
+                "input": {
+                    "training_corpus": training_corpus[:1000],
+                    "vocab": vocab,
+                    "verbose": False,
+                },
+                "expected": {
+                    "len_emission_counts": 442,
+                    "len_transition_counts": 272,
+                    "len_tag_counts": 38,
+                    "emission_counts": {
+                        ("DT", "the"): 48,
+                        ("NNP", "--unk_upper--"): 9,
+                        ("NNS", "Arts"): 1,
+                    },
+                    "transition_counts": {
+                        ("VBN", "TO"): 3,
+                        ("CC", "IN"): 2,
+                        ("VBN", "JJR"): 1,
+                    },
+                    "tag_counts": {"PRP": 11, "UH": 0, ")": 2, },
+                },
+            },
+        ]
+        for test_case in test_cases:
+            result_emission, result_transition, result_tag = target(**test_case["input"])
+            self.assertEqual(True, isinstance(result_emission, defaultdict))
+            self.assertEqual(True, len(result_emission) == test_case["expected"]["len_emission_counts"])
+            for k, v in test_case["expected"]["emission_counts"].items():
+                self.assertEqual(True, np.isclose(result_emission[k], v))
+            self.assertEqual(True, isinstance(result_transition, defaultdict))
+            self.assertEqual (True, len(result_transition) == test_case["expected"]["len_transition_counts"])
+            for k, v in test_case["expected"]["transition_counts"].items():
+                self.assertEqual(True, np.isclose(result_transition[k], v))
+            self.assertEqual(True, isinstance(result_tag, defaultdict))
+            self.assertEqual(True, len(result_tag) == test_case["expected"]["len_tag_counts"])
+            for k, v in test_case["expected"]["tag_counts"].items():
+                self.assertEqual(True, np.isclose(result_tag[k], v))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
